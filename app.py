43588b42c83ffed69f2789e2da245a8164d3b3e4
Anay121 <anay.kulkarni10@gmail.com>
2019-08-15 21:15:45 +0530

    Changes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, session
 from models import RegistrationForm
+from flask_bcrypt import Bcrypt
 import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+brcypt = Bcrypt(app)
 @app.route('/')
 @app.route('/login', methods = ['GET', 'POST'])
 @app.route('/register', methods = ['GET', 'POST'])
@@ -10,9 +12,11 @@ def hello_world():
     form = RegistrationForm(request.form)
     print(request.method)
     if request.method == 'POST' and form.validate():
+        userdet = {}
         for i in form:
-            print(i.data)
+            userdet[i.name] = i.data
         print(request.form)
+        session['user'] = form
         flash('Registered successfully!')
         return render_template('login.html', pageType = str(request.url_rule), form = form) 
     # print(type(request.url_rule), str(request.url_rule))
