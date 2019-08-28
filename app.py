942582d4c7587a97b37f51795db776f05b889af1
Anay121 <anay.kulkarni10@gmail.com>
2019-08-28 13:42:56 +0530

    Added cities list

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import os
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 bcrypt = Bcrypt(app)
+
+cities = ['Adoor', 'Agartala', 'Agra', 'Ahmedabad', 'Aizawl', 'Alappuzha', 'Alleppey', 'Alwar', 'Amravati', 'Amritsar', 'Anand', 'Anantapur', 'Andheri (east)', 'Angamaly', 'Ankleshwar', 'Arpora', 'Asansol', 'Aurangabad', 'Bambolim beach', 'Bangalore', 'Bardez', 'Belgaum', 'Bharuch', 'Bhavnagar', 'Bhopal', 'Bhubaneswar*', 'Bhuj', 'Bikaner', 'Bilaspur', 'Bishnupur', 'Burdwan', 'Calangute', 'Calicut', 'Canacona', 'Candolim', 'Cansaulim', 'Chalakudy', 'Chandigarh', 'Chandigarh*', 'Changanassery', 'Chennai*', 'Cherai', 'Cherthala', 'Chirang', 'Chittur-thathamangalam', 'Coimbatore', 'Coonoor', 'Dalhousie', 'Daman', 'Damthang', 'Daporijo', 'Darjiling', 'Dehradun', 'Delhi', 'Desuri', 'Dhanbad', 'Dhar', 'Dholpur', 'Dibrugarh', 'Dindigul', 'Dona paula', 'Durg-bhilai nagar', 'Durgapur', 'Dwarka', 'Ernakulam', 'Ettumanoor', 'Faridabad', 'Gandhidham', 'Gandhinagar', 'Gangtok', 'Gaziyabad', 'Ghaziabad', 'Golaghat', 'Gondia', 'Guntur', 'Gurgaon', 'Guruvayoor', 'Guwahati', 'Gwalior', 'Havelock island', 'Hyderabad', 'Idduki', 'Idukki', 'Imphal*', 'Indore', 'Itanagar', 'Jabalpur', 'Jaipur', 'Jaisalmer', 'Jalandhar', 'Jalpaiguri', 'Jamnagar', 'Jamshedpur', 'Jhunjhunu', 'Jodhpur', 'Junagadh', 'Kakinada', 'Kakkayangad', 'Kalady', 'Kancheepuram', 'Kanchipuram', 'Kanhangad', 'Kannur', 'Kanpur', 'Karauli', 'Karjat', 'Kasaragod', 'Kasargod', 'Kasauli', 'Kayamkulam', 'Khajuraho', 'Kochi', 'Kodaikanal', 'Kodungallur', 'Kokrajhar', 'Kolkata', 'Kollam', 'Kollhapur', 'Korba', 'Kottayam', 'Kovalam', 'Koyilandy', 'Kozhikode', 'Kumarakom', 'Kunnamkulam', 'Kuttiady', 'Ladnu', 'Lonavala', 'Lonavla', 'Lucknow*', 'Ludhiana', 'Madikeri', 'Madurai', 'Mahabaleshwar', 'Mahaballipuram', 'Mahesana', 'Malappuram', 'Mangalore', 'Mapusa', 'Margao', 'Mavelikkara', 'Moradabad', 'Morvi', 'Mumbai', 'Mundra', 'Munnar', 'Mussoorie', 'Muvattupuzha', 'Mysore', 'Nadiad', 'Nagaon', 'Nagpur', 'Naharlagun', 'Nashik', 'Navi mumbai', 'Nedumangad', 'Nedumbassery', 'New delhi', 'Nilambur', 'Nilgiris', 'Noida', 'Ooty', 'Ottappalam', 'Palai', 'Palakkad', 'Palghar', 'Pali', 'Panaji*', 'Panamattom', 'Panipat', 'Paravoor', 'Pasighat', 'Pathanamthitta', 'Patna*', 'Payyanur', 'Pelling', 'Peravoor', 'Perinthalmanna', 'Perumbavoor', 'Porbandar', 'Port blair*', 'Punalur', 'Pune', 'Puri', 'Purulia', 'Rabong', 'Raigad', 'Raigarh', 'Raipur*', 'Rajahmundry', 'Rajam', 'Rajkot', 'Rajnandgaon', 'Rajsamand', 'Rajula', 'Ranchi*', 'Ranga reddy', 'Ratnagiri', 'Salcete', 'Salcette', 'Salem', 'Saurashtra', 'Sawai madhopur', 'Secunderabad', 'Seoni', 'Shillong*', 'Shimla*', 'Shirdi', 'Shoranur', 'Sikar', 'Solan', 'Solapur', 'Soreng', 'Sultan bathery', 'Surat', 'Taliparamba', 'Tenali', 'Thane', 'Thanjavur', 'Thekkady', 'Theni allinagaram', 'Thiruvalla', 'Thiruvananthapuram', 'Thodupuzha', 'Thrissur', 'Tinsukia', 'Tiruchirappalli', 'Tirupati', 'Tirur', 'Trichy', 'Udaipur', 'Ujjain', 'Vadakara', 'Vadodara', 'Vaikom', 'Vapi', 'Varanasi', 'Varkala', 'Veraval', 'Vijayawada', 'Visakhapatnam', 'Wayanad', 'West kameng', 'Zirakpur']
+
 @app.route('/')
 @app.route('/login', methods = ['GET', 'POST'])
 @app.route('/register', methods = ['GET', 'POST'])
@@ -23,7 +26,7 @@ def hello_world():
 
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', cities = cities)
 
 @app.route('/logout')
 def logout():
